36469100da090b2eee91dd56a151c2b42f18e9ec
rudgas <birk.urmersbach@gmx.de>
2018-10-27 21:28:16 +0200

    Further cleaning up code, possible extension towards manual choice of number of tiles

--- Superresolution.py
+++ Superresolution.py
@@ -43,16 +43,20 @@ def resize():
 		out = Image.fromarray(npimage,mode="RGB")
 		out = out.resize((w*2,h*2), Image.BILINEAR) #Alternatives: Image.LANCZOS Image.BILINEAR Image.NEAREST
 		out.save("./resized/" + imlist[i][:-4] + "_resized.tif", format='TIFF', compression='None') #tiff_lzw
+	#return to parent directory
+	os.chdir("..")
 
 def align():
 	#use hugin for alignment of images
-	os.chdir("./resized")
+	os.chdir("./process/resized")
 	os.system("align_image_stack --use-given-order -C -x -y -z -a aligned_ --corr=0.8 -t 1 -c 100 -v --gpu *.tif")
 	os.system("rm *_resized.tif")
+	os.chdir("../..")
 
 def tile():
+	os.chdir("./process/resized")
 	#create folders for the 4 tiles
-	for i in range(1,5):
+	for i in range(4):
 		try:
 			os.mkdir("tile"+str(i))
 		except:
@@ -75,31 +79,28 @@ def tile():
 		#calculate boundaries for four tiles
 		tdimw = int(width/2)
 		tdimh = int(height/2)
-		tile1 = npimage[:tdimh,:tdimw,:]
-		tile2 = npimage[tdimh:,:tdimw,:]
-		tile3 = npimage[:tdimh,tdimw:,:]
-		tile4 = npimage[tdimh:,tdimw:,:]
 		
-		#save and resize tiles
-		outtile1 = Image.fromarray(tile1,mode="RGB")
-		outtile1.save("./tile1/" + imlist[i][:-4] + "_tile1.tif", format='TIFF', compression='None')
+		tile = np.zeros((tdimh,tdimw,3,4),dtype=np.uint8)
 		
-		outtile2 = Image.fromarray(tile2,mode="RGB")
-		outtile2.save("./tile2/" + imlist[i][:-4] + "_tile2.tif", format='TIFF', compression='None')
-
-		outtile3 = Image.fromarray(tile3,mode="RGB")
-		outtile3.save("./tile3/" + imlist[i][:-4] + "_tile3.tif", format='TIFF', compression='None')
-
-		outtile4 = Image.fromarray(tile4,mode="RGB")
-		outtile4.save("./tile4/" + imlist[i][:-4] + "_tile4.tif", format='TIFF', compression='None')
-	
+		tile[:,:,:,0] = npimage[:tdimh,:tdimw,:]
+		tile[:,:,:,1] = npimage[tdimh:,:tdimw,:]
+		tile[:,:,:,2] = npimage[:tdimh,tdimw:,:]
+		tile[:,:,:,3] = npimage[tdimh:,tdimw:,:]
+		
+		
+		for j in range(0,4):
+			outtile = Image.fromarray(tile[:,:,:,j],mode="RGB")
+			outtile.save("./tile" + str(j) + "/" + imlist[i][:-4] + "_tile" + str(j) + ".tif", format='TIFF', compression='None')
+		
 	#remove aligned images
 	os.system("rm align*")
+	os.chdir("../..")
 	
 def average():
 	#calculate average values for each pixel
+	os.chdir("./process/resized")
 	print("Calculating averages")
-	for i in range(1,5):
+	for i in range(4):
 		os.chdir("./tile"+str(i))
 		
 		allfiles = os.listdir()
@@ -117,10 +118,12 @@ def average():
 		outpooltile = Image.fromarray(pooltile,mode="RGB")
 		outpooltile.save("avg_tile" + str(i) + ".tif", format='TIFF', compression='None')
 		os.chdir("..")
+	os.chdir("../..")
 		
 def median():
+	os.chdir("./process/resized")
 	print("Calculating median values")
-	for i in range(1,5):
+	for i in range(4):
 		os.chdir("./tile"+str(i))
 		
 		allfiles = os.listdir()
@@ -147,52 +150,45 @@ def median():
 		poolmedian = np.array(np.rint(poolmedian),dtype=np.uint8)
 		outpoolmedian = Image.fromarray(poolmedian,mode="RGB")
 		outpoolmedian.save("median_tile" + str(i) + ".tif", format='TIFF', compression='None')
-		os.chdir("..")		
+		os.chdir("..")
+	os.chdir("../..")
 		
 def stitch():
+	try: 
+		os.mkdir("./process/superresolution")
+	except:
+		print("folder present")
+	os.chdir("./process/resized")
 	print("Stitching everything together")
-	os.mkdir("../superresolution")
 	
-	#stitch average
-	tile1 = Image.open("./tile1/avg_tile1.tif")
-	tile2 = Image.open("./tile2/avg_tile2.tif")
-	tile3 = Image.open("./tile3/avg_tile3.tif")
-	tile4 = Image.open("./tile4/avg_tile4.tif")
+	#combination
+	methods = ["avg", "median"]
 	
-	#in case the original has an uneven number of pixels along an axis, we check three tiles
-	w1, h1 = tile1.size
-	w2, h2 = tile2.size
-	w3, h3 = tile3.size
-	
-	#create merged image
-	merged = Image.new('RGB', (w1 + w3, h1 + h2))
-	merged.paste(im = tile1, box=(0,0))
-	merged.paste(im = tile2, box=(0,h1))
-	merged.paste(im = tile3, box=(w1,0))
-	merged.paste(im = tile4, box=(w1,h1))
-	
-	merged.save("../superresolution/avg_result.tif", format='TIFF', compression='None')
+	for i in methods:
+		#load tiles
+		tile0 = Image.open("./tile0/" + i + "_tile0.tif")
+		tile1 = Image.open("./tile1/" + i + "_tile1.tif")
+		tile2 = Image.open("./tile2/" + i + "_tile2.tif")
+		tile3 = Image.open("./tile3/" + i + "_tile3.tif")
+		
+		#in case the original has an uneven number of pixels along an axis, we use all three tiles
+		w1, h1 = tile0.size
+		w2, h2 = tile1.size
+		w3, h3 = tile2.size
+		
+		#create merged image
+		merged = Image.new('RGB', (w1 + w3, h1 + h2))
+		merged.paste(im = tile0, box=(0,0))
+		merged.paste(im = tile1, box=(0,h1))
+		merged.paste(im = tile2, box=(w1,0))
+		merged.paste(im = tile3, box=(w1,h1))
+		
+		merged.save("../superresolution/" + i + "_result.tif", format='TIFF', compression='None')
 
-	#stitch median
-	tile1 = Image.open("./tile1/median_tile1.tif")
-	tile2 = Image.open("./tile2/median_tile2.tif")
-	tile3 = Image.open("./tile3/median_tile3.tif")
-	tile4 = Image.open("./tile4/median_tile4.tif")
-	
-	w1, h1 = tile1.size
-	w2, h2 = tile2.size
-	w3, h3 = tile3.size
-	
-	merged = Image.new('RGB', (w1 + w3, h1 + h2))
-	merged.paste(im = tile1, box=(0,0))
-	merged.paste(im = tile2, box=(0,h1))
-	merged.paste(im = tile3, box=(w1,0))
-	merged.paste(im = tile4, box=(w1,h1))
-	
-	merged.save("../superresolution/median_result.tif", format='TIFF', compression='None')
-	
 	#remove merged tiles
-	os.system("rm -r ../resized")
+	os.chdir("../..")
+	os.system("rm -r ./process/resized")
+	
 	
 if __name__ == '__main__':
 	main()
